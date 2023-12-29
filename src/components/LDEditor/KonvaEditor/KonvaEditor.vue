0f714867ed4ff37b174c8ac5be2ad0192edf2946
qinhj <qhj@systech.com.cn>
2023-12-29 09:56:25 +0800

    feat: 实现动态绘制功能块

--- src/components/LDEditor/KonvaEditor/KonvaEditor.vue
+++ src/components/LDEditor/KonvaEditor/KonvaEditor.vue
@@ -4,6 +4,12 @@ import { onMounted } from 'vue';
 
 import { Text } from './Text';
 
+// import { Instruction } from '../type';
+
+import instructList from '../instructList.json';
+
+console.log(instructList);
+
 let currentText: Konva.Text;
 
 let input: HTMLInputElement;
@@ -27,62 +33,98 @@ onMounted(() => {
     // draggable: true,
   });
 
-  //一个group代表一个指令集，包含文字、线条、矩形等
-  const group = new Konva.Group({
-    // draggable: true,
-  });
+  /** 矩形宽度 */
+  const rectWidth = 180;
 
-  // 绘制矩形
-  const Rect = new Konva.Rect({
-    x: 100,
-    y: 100,
-    width: 180,
-    height: (4 * 100) / 2,
-    stroke: 'black',
-    strokeWidth: 1,
-  });
+  /** 矩形间隔(水平) */
+  const rectGap = 180;
 
-  // add the shape to the layer
-  group.add(Rect);
+  /** 参数间隔(垂直) */
+  const interval = 100;
 
-  group.add(
-    new Text(
-      {
-        x: 100,
-        y: 100 - 18,
-        align: 'center',
-        text: '指令一',
-      },
-      Rect,
-      input,
-      (Text) => (currentText = Text),
-    ),
-  );
+  /** 参数距离顶部和底部的距离 */
+  const verticalGap = interval / 2;
 
-  for (let i = 0; i < 4; i++) {
-    const isLeft = i % 2 === 0;
+  // 遍历指令集列表
+  for (let insIndex = 0; insIndex < instructList.length; insIndex++) {
+    const instruct = instructList[insIndex];
+    if (instruct.InsType !== 3) continue;
+
+    // 功能块起始坐标
+    const startX = 100 + insIndex * (rectWidth + rectGap);
+    const startY = 100;
+
+    const group = new Konva.Group({
+      draggable: true,
+    });
+
+    layer.add(group);
+
+    const rectHeight = (interval / 2) * instruct.ParamsNumber + verticalGap;
+
+    // 绘制矩形
+    const Rect = new Konva.Rect({
+      x: startX,
+      y: startY,
+      width: rectWidth,
+      height: rectHeight,
+      stroke: 'black',
+      strokeWidth: 1,
+    });
+    group.add(Rect);
+
+    // 绘制标题
     group.add(
       new Text(
         {
-          x: isLeft ? 100 - 180 : 100 + 180,
-          y: 100 + 50 + 100 * parseInt(i / 2 + ''),
-          align: i % 2 === 0 ? 'right' : 'left',
-          text: `参数${i}`,
+          x: startX,
+          y: startY - 18,
+          align: 'center',
+          text: instruct.InsName,
         },
         Rect,
         input,
         (Text) => (currentText = Text),
       ),
     );
+
+    // 上一个参数类型
+    let lastParamsType = 0;
+
+    // 同一类型参数的下标
+    let sameParamsTypeCount = 0;
+
+    // 绘制参数
+    for (
+      let paramsIndex = 0;
+      paramsIndex < instruct.Params.length;
+      paramsIndex++
+    ) {
+      const params = instruct.Params[paramsIndex];
+
+      if (lastParamsType === params.ParamType) sameParamsTypeCount++;
+      else sameParamsTypeCount = 0;
+
+      lastParamsType = params.ParamType;
+
+      const isLeft = params.ParamType === 1;
+      group.add(
+        new Text(
+          {
+            x: startX + rectWidth * (isLeft ? -1 : 1),
+            y: startY + verticalGap + interval * sameParamsTypeCount,
+            align: isLeft ? 'right' : 'left',
+            text: params.ParamName as string,
+          },
+          Rect,
+          input,
+          (Text) => (currentText = Text),
+        ),
+      );
+    }
   }
 
-  // add the layer to the stage
-
-  layer.add(group);
   stage.add(layer);
-
-  // draw the image
-  // layer.draw();
 });
 </script>
 
