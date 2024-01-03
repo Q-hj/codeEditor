49a192b4678ef6e76d9921907ba4079d4b86e1de
yangsq <ysq@systech.com.cn>
2024-01-03 16:56:04 +0800

    feat:添加 webview2 类型文件、主机对象接口示例

---
+++ src/controllers/instructionController.ts
@@ -0,0 +1,7 @@
+import type { HostObjectAsyncProxy } from '@/types/webview2'
+
+interface InstructionController extends HostObjectAsyncProxy{
+  GetLDInstructions():Promise<string>
+}
+
+export const instructionController =  window.chrome.webview.hostObjects.instructionController as InstructionController;
